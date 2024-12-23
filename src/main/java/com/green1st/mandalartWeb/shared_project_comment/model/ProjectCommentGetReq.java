4ac8dd7421f5be68c538d908fd00499078f7b011
KwonHyeji <kwonhyeji007@gmail.com>
2024-12-23 17:59:15 +0900

    model 일단 완료

--- src/main/java/com/green1st/mandalartWeb/shared_project_comment/model/ProjectCommentGetReq.java
+++ src/main/java/com/green1st/mandalartWeb/shared_project_comment/model/ProjectCommentGetReq.java
@@ -1,4 +1,23 @@
 package com.green1st.mandalartWeb.shared_project_comment.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.beans.ConstructorProperties;
+
+@Getter
+@Schema(title = "공유 프로젝트 댓글 리스트 요청")
+@ToString
 public class ProjectCommentGetReq {
+
+    @Schema(title = "프로젝트 PK", description = "프로젝트 PK", name = "project_id", example = "1"
+            , requiredMode = Schema.RequiredMode.REQUIRED)
+    private long projectId;
+
+
+    @ConstructorProperties({"project_id"})
+    public ProjectCommentGetReq(long projectId) {
+        this.projectId = projectId;
+    }
 }
