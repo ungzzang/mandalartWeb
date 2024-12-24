455f6e3721fa0d8c22d75cc802b29bb00398e952
LimHyeonChan <lhc0240@gmail.com>
2024-12-24 17:53:06 +0900

    줄 없앰

--- src/main/java/com/green1st/mandalartWeb/mandalart/MandalartService.java
+++ src/main/java/com/green1st/mandalartWeb/mandalart/MandalartService.java
@@ -62,7 +62,9 @@ public class MandalartService {
             }
 
             // 완료율 계산
-            double completionRate = (double) completedCount / dtoList.size();
+            double completionRate = (dtoList != null && !dtoList.isEmpty())
+                    ? (double) completedCount / dtoList.size()
+                    : 0;
 
             // 색상 계산
             String color = calculateColorCode(item.getDepth(), completionRate, colorCodes);
