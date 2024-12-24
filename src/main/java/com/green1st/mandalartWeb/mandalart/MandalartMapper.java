35f8e38e91d583889189036a3d1e609822954cf5
LimHyeonChan <lhc0240@gmail.com>
2024-12-24 09:55:36 +0900

    컨트롤 수정본

--- src/main/java/com/green1st/mandalartWeb/mandalart/MandalartMapper.java
+++ src/main/java/com/green1st/mandalartWeb/mandalart/MandalartMapper.java
@@ -1,14 +1,14 @@
 package com.green1st.mandalartWeb.mandalart;
 
 
-import com.green1st.mandalartWeb.mandalart.model.*;
+//import com.green1st.mandalartWeb.mandalart.model.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface MandalartMapper {
-    List<MandalartGetRes> getMandalart (MandalartGetReq p);
-    List<MandalartPostDto>  patchMand (MandalartPostReq p);
+//    List<MandalartGetRes> getMandalart (MandalartGetReq p);
+//    List<MandalartPostDto>  patchMand (MandalartPostReq p);
 
 }
