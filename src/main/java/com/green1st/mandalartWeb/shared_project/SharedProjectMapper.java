4ff2515e89e90b2155b47a0bd642e97a3142600f
SugiSagong <jumoney1012@gmail.com>
2024-12-26 14:30:29 +0900

    프로젝트 생성, 조회, 공유 프로젝트 등록 완료 공유 프로젝트 조회 완료

--- src/main/java/com/green1st/mandalartWeb/shared_project/SharedProjectMapper.java
+++ src/main/java/com/green1st/mandalartWeb/shared_project/SharedProjectMapper.java
@@ -1,10 +1,15 @@
 package com.green1st.mandalartWeb.shared_project;
 
+import com.green1st.mandalartWeb.shared_project.model.SharedProjectGetReq;
+import com.green1st.mandalartWeb.shared_project.model.SharedProjectGetRes;
 import com.green1st.mandalartWeb.shared_project.model.SharedProjectPostReq;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SharedProjectMapper {
     int insSharedProject(SharedProjectPostReq p);
+    List<SharedProjectGetRes> selSharedProjectList(SharedProjectGetReq p);
 }
--- src/main/java/com/green1st/mandalartWeb/shared_project/SharedProjectService.java
+++ src/main/java/com/green1st/mandalartWeb/shared_project/SharedProjectService.java
@@ -2,12 +2,20 @@ package com.green1st.mandalartWeb.shared_project;
 
 import com.green1st.mandalartWeb.common.model.ResultResponse;
 import com.green1st.mandalartWeb.project.ProjectMapper;
+import com.green1st.mandalartWeb.project.model.ProjectGetReq;
+import com.green1st.mandalartWeb.project.model.ProjectGetRes;
 import com.green1st.mandalartWeb.project.model.ProjectResultDto;
 import com.green1st.mandalartWeb.shared_project.model.ProjectSelDto;
+import com.green1st.mandalartWeb.shared_project.model.SharedProjectGetReq;
+import com.green1st.mandalartWeb.shared_project.model.SharedProjectGetRes;
 import com.green1st.mandalartWeb.shared_project.model.SharedProjectPostReq;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SharedProjectService {
@@ -39,5 +47,16 @@ public class SharedProjectService {
                 .resultMsg("공유 프로젝트 등록 실패")
                 .build();
 
+    }
+
+    public ResultResponse<?> getSaherdProject(SharedProjectGetReq p) {
+        log.info("asdsadadasd {}", p);
+        List<SharedProjectGetRes> sharedProjectList = sharedProjectMapper.selSharedProjectList(p);
+
+        return ResultResponse.<List<SharedProjectGetRes>>builder()
+                .statusCode("200")
+                .resultData(sharedProjectList)
+                .resultMsg("공유 프로젝트 조회 완료")
+                .build();
     }
 }
