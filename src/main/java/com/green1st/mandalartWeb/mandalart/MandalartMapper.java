7421ae0d1c796c080f4447bece08b12107daefc9
LimHyeonChan <lhc0240@gmail.com>
2024-12-27 16:47:11 +0900

    xml 추가

--- src/main/java/com/green1st/mandalartWeb/mandalart/MandalartMapper.java
+++ src/main/java/com/green1st/mandalartWeb/mandalart/MandalartMapper.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface MandalartMapper {
     List<MandalartGetRes> getMandalart (MandalartGetReq p);
     int patchMandalart (MandalartPostReq p);
-    imminentMandalart
+
 }
