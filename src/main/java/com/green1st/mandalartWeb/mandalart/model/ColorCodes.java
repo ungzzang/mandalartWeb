d65651e7eefd6a9a446b89d4569b7aadd87eddc8
SugiSagong <jumoney1012@gmail.com>
2025-01-04 15:48:01 +0900

    fix: 완료 색상 변경

--- src/main/java/com/green1st/mandalartWeb/mandalart/model/ColorCodes.java
+++ src/main/java/com/green1st/mandalartWeb/mandalart/model/ColorCodes.java
@@ -11,14 +11,14 @@ import java.util.List;
 public class ColorCodes {
     // 0레벨 색상
     private  List<String> titleColor = Arrays.asList(
-            "#001A00", "#003300", "#004C00", "#006600", "#007F00", "#339933", "#66CC66", "#99FF99"
+            "#55ad9a4a", "#55ad9a73", "#55ad9a8a", "#55ad9a9c", "#55ad9ab3", "#55ad9ac9", "#55ad9ae0", "#55ad9a"
     );
     // 1레벨 색상
     private  List<String> subTitleColor = Arrays.asList(
-            "#99FF99FF", "#99FF99E6", "#99FF99CC", "#99FF99B3", "#99FF9999", "#99FF997F", "#99FF9966", "#99FF994C"
+            "#68a99b24", "#68a99b3b", "#68a99b54", "#68a99b6b", "#68a99b87", "#68a99bb0", "#68a99bdb", "#68a99b"
     );
 
-    private  List<String> defaultColor = Arrays.asList("#FFFFFF");  // 리스트로 변경
+    private  List<String> defaultColor = Arrays.asList("#88aba3");  // 리스트로 변경
 
     public List<String> getTitleColor() {
         return titleColor;
